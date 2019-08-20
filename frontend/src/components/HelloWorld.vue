fe96e2c36d48a66a0052b0f766caf17c145dadff
shoichi-sasaki <shoichi-sasaki@x-power.co.jp>
2019-08-20 19:44:01 +0900

    frontend 作成１

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,32 +1,43 @@
 <template>
-  <v-container height="20000px">
+  <v-container class="pa-0" align-center>
     <!-- <v-layout text-center wrap></v-layout> -->
     <v-parallax dark height="300" src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
       <v-layout align-center column justify-center>
         <h1 class="display-2 font-weight-thin mb-10 mt-10">Summer Intern2019</h1>
-        <div class="mb-4">
-          <input type="text" v-model="word" placeholder="Here comes a word"/>
-          <v-btn depressed small @click="request" :loading="loading" color="primary">search</v-btn>
-        </div>
+        <v-flex>
+          <v-text-field label="Here comes a word" outlined v-model="word"></v-text-field>
+          <v-btn
+            large
+            depressed
+            small
+            @click="request"
+            :loading="loading"
+            :disabled="loading"
+            color="primary"
+          >search</v-btn>
+        </v-flex>
       </v-layout>
     </v-parallax>
     <v-layout>
-      <v-flex md12 text-center>
-        <div class="my-2">{{datas}}</div>
+      <v-flex md12 text-center v-if="datas">
+            <item-box :product_info="item" v-for="(item,index) in datas[1]['item']" :key="index"></item-box>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
+import ItemBox from "./ItemBox.vue";
 import axios from "axios";
 export default {
   data: () => ({
-    message: "Hello Vue!",
     datas: null,
     word: "",
     loading: false
   }),
+  components: {
+    "item-box": ItemBox
+  },
   methods: {
     request: function() {
       this.loading = true;
---
+++ frontend/src/components/ItemBox.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container class="pa-0 item-box">
+    <v-layout class="d-block">
+      <v-flex pa-0>
+        <v-img
+          :src="product_info['image']"
+          aspect-ratio="1"
+          class="grey lighten-2"
+          max-height="160"
+          max-width="160"
+          position="top"
+        ></v-img>
+      </v-flex>
+      <v-flex pa-2 class="product-detail">
+          <h5 class="product-title">{{product_info['title']}}</h5>
+          <div class="mt-1" >¥{{product_info['price']}}</div>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+<script>
+export default {
+  props: ["product_info"],
+  data: () => ({
+    message: "herehere"
+  })
+};
+</script>
+<style>
+.item-box{
+    width:160px !important;
+    height:300px !important;
+}
+.product-detail{
+    background-color:white;
+}
+.product-title{
+    height:65px;
+    overflow:hidden;
+}
+</style>

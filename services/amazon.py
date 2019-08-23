309c09a93f8db9244a34dbc6eb548db705d3bd69
shoichi-sasaki <shoichi-sasaki@x-power.co.jp>
2019-08-23 15:52:16 +0900

    バックエンド整理

--- services/amazon.py
+++ services/amazon.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 
 def get_product_info(search_word):
-    url = "https://www.amazon.co.jp/s?k="
+    url = "https://www.amazon.co.jp/s"
     query = {'k': search_word}
     headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
     # htmlのソースコードを取得
